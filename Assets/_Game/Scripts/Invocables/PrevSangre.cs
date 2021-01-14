ebae907b4560bb321850fd986e2bf3af8c69dbdf
Morion <adrian.martinez@udea.edu.co>
2021-01-14 18:00:42 -0500

    Escena de David

--- Assets/_Game/Scripts/Invocables/PrevSangre.cs
+++ Assets/_Game/Scripts/Invocables/PrevSangre.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PrevSangre : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image imSangre;
+    IEnumerator Start()
     {
-        
+        yield return new WaitForSeconds(0.1f);
+        Vida m = GetComponentInParent<Vida>();
+        if (m!=null && m.imSangre == null)
+        {
+            m.imSangre = imSangre;
+        }
     }
 
     // Update is called once per frame
